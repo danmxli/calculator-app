1c0d7e7de551a87f2f01df3591d19815ecee08df
Dan Li <danmuxingli@gmail.com>
2023-01-01 17:25:12 -0500

    added skeleton arithmetic functions to root app

---
+++ calculator.py
@@ -0,0 +1,45 @@
+# import tkinter library
+from tkinter import *
+
+# root widget
+root = Tk()
+
+# entry widget for user
+userEntry = Entry(root, width=50, borderwidth=7)
+
+# error handling invalid user input function
+
+# do operation function definition, only two parameters
+def equalOperation():
+    global userInput
+    global operationSign
+
+# arithmetic sign definition
+def addSign():
+    global userInput
+    global operationSign
+
+def subtractSign():
+    global userInput
+    global operationSign
+
+def multiplySign():
+    global userInput
+    global operationSign
+
+def divideSign():
+    global userInput
+    global operationSign
+
+def expSign():
+    global userInput
+    global operationSign
+
+def sqrtSign():
+    global userInput
+    global operationSign
+
+# buttons
+
+# run app
+root.mainloop()
