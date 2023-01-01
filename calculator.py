9b26ea634e2b36a929c9d7c11403f8324268a458
Dan Li <danmuxingli@gmail.com>
2023-01-01 02:39:05 -0500

    create button widget with function

--- calculator.py
+++ calculator.py
@@ -4,13 +4,22 @@ from tkinter import *
 # create root wiget first 
 root = Tk()
 
-# initialize wiget
+#define commands
+def clickMe():
+    myLabel = Label(root, text = "button clicked")
+    myLabel.grid()
+
+# initialize label 
 labelHello = Label(root, text="Hello World!")
 labelTwo = Label(root, text="Label two")
 
+# initialize button
+buttonOne = Button(root, text="me", command=clickMe)
+
 # put into root using .grid row col
 labelHello.grid(row=1, column=2)
-labelTwo.grid(row=2, column=1)
+labelTwo.grid(row=1, column=1)
+buttonOne.grid(row=2, column=2)
 
 # run the wiget
 root.mainloop()
